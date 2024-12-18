175c9efbad743868c30bb345f67b7a469c73e69e
Nicolas Boisvert <nboisvert@StudioMini.local>
2024-12-17 23:36:14 -0500

    Remove commited code

--- Sources/entrypoint.swift
+++ Sources/entrypoint.swift
@@ -35,31 +35,3 @@ struct Advent2024: ParsableCommand {
         print("Result \(result)")
     }
 }
-//
-// let input = "input.txt"
-// let numberPair = /([0-9]+)\s+([0-9]+)/
-//
-// var leftNumbers: [Int] = []
-// var rightNumbers: [Int] = []
-//
-// do {
-//     let content = try String(contentsOfFile: input).split { $0.isNewline }
-//     for pair in content {
-//         if let match = pair.firstMatch(of: numberPair) {
-//             let left = Int(match.1)!
-//             let right = Int(match.2)!
-//             leftNumbers.append(left)
-//             rightNumbers.append(right)
-//         }
-//     }
-//     leftNumbers.sort()
-//     rightNumbers.sort()
-//     var accumulator = 0
-//     for (index, leftNumber) in leftNumbers.enumerated() {
-//         let rightNumber = rightNumbers[index]
-//         accumulator = accumulator + abs(leftNumber - rightNumber)
-//     }
-//     print(accumulator)
-// } catch {
-//     print(error)
-// }
